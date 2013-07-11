99591cc1efcab3be277768d13f6223c8325d01e4
Gaurav Chaudhary <gaurav@gauravchaudhary.com>
2013-07-11 00:04:49 +0000

    Added parsing by url function and json output

--- grader.js
+++ grader.js
@@ -22,6 +22,8 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
+var sys = require('util');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = 'index.html';
@@ -36,6 +38,7 @@ var assertFileExists = function(inFile) {
     return instr;
 };
 
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
@@ -43,16 +46,30 @@ var cheerioHtmlFile = function(htmlfile) {
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
- 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+
+var checkMarkup = function(checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for (var ii in checks) {
 	var present = $(checks[ii]).length > 0;
 	out[checks[ii]] = present;
+     }
+    var outJson = JSON.stringify(out, null, 4);
+    console.log(outJson);
+}; 
+
+var checkHtmlFile = function(htmlfile, checksfile, url) {
+    
+    if (url) {
+	rest.get(url).on('complete', function(result) {
+            $ = cheerio.load(result);            
+            checkMarkup(checksfile);
+	});
+    } else {
+	$ = cheerioHtmlFile(htmlfile);
+	checkMarkup(checksfile);
     }
-    return out;
+
 };
 
 var clone = function(fn) {
@@ -65,11 +82,11 @@ if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_html_file>', 'URL to index.html') 
 	.parse(process.argv);
 
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    checkHtmlFile(program.file, program.checks, program.url);
+    
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
